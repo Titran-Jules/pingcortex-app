ca0add9b6821905e3992798d81743056dccde36b
JulesTitran <titranj@gmail.com>
2026-09-26 18:00:43 +0300

    feat(api): add user api with getProfile, getApiKeys and addApiKey

--- packages/api-client/src/api.ts
+++ packages/api-client/src/api.ts
@@ -1,4 +1,4 @@
-import { LoginRequest, LoginResponse, UserRegister, UserResponse } from "@pingcortex/shared-types";
+import { ApiKeyCreate, ApiKeyResponse, LoginRequest, LoginResponse, UserRegister, UserResponse } from "@pingcortex/shared-types";
 import { AxiosInstance } from "axios";
 
 export class PingCortexApi {
@@ -14,4 +14,15 @@ export class PingCortexApi {
         logout: (refreshToken: string) =>
             this.client.post<void>('/auth/logout', { refreshToken }),
     };
+
+    user = {
+        getProfile: () =>
+            this.client.get<UserResponse>('/users/me').then((res) => res.data),
+
+        getApiKeys: () =>
+            this.client.get<ApiKeyResponse>('/users/me/api-keys').then((res) => res.data),
+
+        addApiKey: (data: ApiKeyCreate) =>
+            this.client.post<ApiKeyResponse>('/users/me/api-keys', data).then((res) => res.data),
+    }
 }
